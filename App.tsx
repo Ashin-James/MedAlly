2b90d677abf1482feb0d85101415a9cc5c1ff953
Ashin-James <ashinjames.11@gmail.com>
2026-08-01 15:48:13 +0530

    Suppress yellow dev connection warning banners on phone screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { LogBox } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AccessibilityProvider } from './src/context/AccessibilityContext';
+
+// Suppress yellow dev-tool connection warnings on physical device UI
+LogBox.ignoreLogs(['Cannot connect to Expo CLI', 'Expo CLI']);
+LogBox.ignoreAllLogs(true);
 
 export default function App() {
   return (
